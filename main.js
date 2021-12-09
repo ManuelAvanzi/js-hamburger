0ff893a8809f0627a79f0b933eb89c0ce944b787
ManuelAvanzi <90607094+ManuelAvanzi@users.noreply.github.com>
2021-12-09 17:24:52 +0100

    completato

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("This file is not Empty!!!!");
+// apertura menù hamburger da mobile
+
+const hamburgerBtn = document.querySelector('.fa-bars');
+const MenuHamburger = document.querySelector('.hamburger-menu');
+
+
+const close = document.querySelector('.close');
+
+// al click aggiungo la classe active
+hamburgerBtn.addEventListener('click',function(){
+    MenuHamburger.classList.add('active');
+});
+
+// rimuovo la classe active
+close.addEventListener('click',function(){
+    MenuHamburger.classList.remove('active');
+});
